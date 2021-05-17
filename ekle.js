1d26695a04803a4ad38944765e27189f4b59cec9
SalimBerk <salimberk2@gmail.com>
2021-05-17 20:44:53 +0300

    ikinci ödev

---
+++ ekle.js
@@ -0,0 +1,39 @@
+function newElement() {
+  const task = document.querySelector("#task");
+  let message = task.value;
+  let liste = document.querySelector("#list");
+
+  let li = document.createElement("li");
+  li.setAttribute("class", "list-group-item list-group-item-action");
+  li.style.border = "solid #ff7f00";
+  li.style.marginBottom = "50px";
+  li.style.color = "#ff7f00";
+  
+
+  li.innerHTML = message;
+  if (message){
+    liste.appendChild(li);
+  }else{
+    const alert_dom=document.querySelector("#alert");
+    const alert=`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <strong>UYARI!</strong>    Veri girişi yapılmadı !!!.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>`
+    alert_dom.innerHTML=alert;
+    
+
+  }
+
+
+  var span = document.createElement("span");
+  var text = document.createTextNode("X");
+  span.className = "close";
+  span.appendChild(text);
+  li.appendChild(span);
+
+  span.addEventListener("click", function () {
+    liste.removeChild(li);
+  });
+}
